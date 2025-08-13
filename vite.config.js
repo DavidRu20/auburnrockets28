b2fbde6941c270692dd9e3a14718e6a79c61184f
michaelatiemo54-jpg <michaelatiemo54@gmail.com>
2025-08-12 22:47:32 -0400

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // remove if not using React
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/auburnrockets28/',   // <-- replace with your repo name
-})
